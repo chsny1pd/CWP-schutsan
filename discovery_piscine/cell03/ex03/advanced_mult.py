45489fa60266f6e374d0054b78befeb2efb3c81d
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-09-21 21:57:14 +0700

    fixex03

--- discovery_piscine/cell03/ex03/advanced_mult.py
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -1,5 +1,11 @@
-for main_num in range(0, 11):
+main_num = 0
+while main_num <= 10:
     print(f"Table de {main_num}:", end="")
-    for sub_num in range(0, 11):
+        
+    sub_num = 0
+    while sub_num <= 10:
         print(f" {main_num * sub_num}", end="")
+        sub_num += 1
+            
     print()
+    main_num += 1
