81090b72687446b8954d1b0263547d516fbddd0c
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-09-21 21:16:42 +0700

    cell04

---
+++ discovery_piscine/cell04/ex01/age.py
@@ -0,0 +1,5 @@
+age = int(input("Please tell me your age: "))
+
+print(f"You are currently {age} years old.")
+for year in [10, 20, 30]:
+    print(f"In {year} years, you'll be {age + year} years old.")
