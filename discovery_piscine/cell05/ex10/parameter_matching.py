0cd90180a09a04c341dc5fd822852969237f1208
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-09-22 12:16:15 +0700

    fixex10

--- discovery_piscine/cell05/ex10/parameter_matching.py
+++ discovery_piscine/cell05/ex10/parameter_matching.py
@@ -1,10 +1,15 @@
-para_input = input()
+#!/usr/bin/env python3
+import sys
 
-if not para_input:
-    print("none")
-else:
+# เช็คว่ามีพารามิเตอร์ส่งมา 1 ตัวพอดีหรือไม่ (เมื่อรวมชื่อไฟล์แล้ว ความยาวต้องเท่ากับ 2)
+if len(sys.argv) == 2:
+    # โปรแกรมแสดงข้อความถามและรอรับคำตอบจากผู้ใช้
     text_input = input("What was the parameter? ")
-    if para_input == text_input:
+    
+    # นำคำตอบมาเทียบกับพารามิเตอร์ตอนที่สั่งรันโปรแกรม (sys.argv[1])
+    if sys.argv[1] == text_input:
         print("Good job!")
     else:
         print("Nope, sorry...")
+else:
+    print("none")
